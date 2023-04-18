8f715c43a91c6751e8fd3e5e4d8d950f63b307cb
Jonathan Good <github@bungogood.com>
2023-04-18 13:56:12 +0100

    Removing State struct as only 2 elements

--- src/solver/quick.rs
+++ src/solver/quick.rs
@@ -3,12 +3,6 @@ use std::vec;
 use crate::solver::{Neighbours, Solver};
 use crate::sudoku::Sudoku;
 
-struct State {
-    worklist: Vec<(usize, u8)>,
-    // connections: Vec<Vec<usize>>,
-    possible: Vec<Vec<u8>>,
-}
-
 pub struct Quick {
     connections: Vec<Vec<usize>>,
 }
@@ -21,17 +15,16 @@ impl Quick {
         }
     }
 
-    fn eliminate(&self, state: &mut State) -> bool {
-        while let Some((x, p)) = state.worklist.pop() {
+    fn eliminate(&self, worklist: &mut Vec<(usize, u8)>, possible:&mut Vec<Vec<u8>>) -> bool {
+        while let Some((x, p)) = worklist.pop() {
             for &n in self.connections[x].iter() {
-                if state.possible[n].len() == 1 {
+                if possible[n].len() == 1 {
                     continue;
                 }
-                state.possible[n].retain(|&v| v != p);
-                // state.connections[n].retain(|&i| i != x);
-                match state.possible[n][..] {
+                possible[n].retain(|&v| v != p);
+                match possible[n][..] {
                     [] => return false,
-                    [v] => state.worklist.push((n, v)),
+                    [v] => worklist.push((n, v)),
                     _ => {}
                 }
             }
@@ -39,28 +32,23 @@ impl Quick {
         true
     }
 
-    fn proc(&self, state: &mut State) -> Option<Vec<u8>> {
-        if !self.eliminate(state) {
+    fn proc(&self, worklist: &mut Vec<(usize, u8)>, possible:&mut Vec<Vec<u8>>) -> Option<Vec<u8>> {
+        if !self.eliminate(worklist, possible) {
             return None;
         }
 
-        match state
-            .possible
+        match possible
             .iter()
             .enumerate()
             .filter(|(_, v)| v.len() > 1)
             .min_by_key(|(_, v)| v.len())
         {
-            None => return Some(state.possible.iter().map(|v| v[0]).collect()),
+            None => return Some(possible.iter().map(|v| v[0]).collect()),
             Some((shortest, pos)) => {
                 for &v in pos {
-                    let mut dup = State {
-                        worklist: vec![(shortest, v)], // all filled squared
-                        // connections: state.connections.clone(),
-                        possible: state.possible.clone(),
-                    };
-                    dup.possible[shortest] = vec![v];
-                    if let Some(pos) = self.proc(&mut dup) {
+                    let mut replica = &mut possible.clone();
+                    replica[shortest] = vec![v];
+                    if let Some(pos) = self.proc(&mut vec![(shortest, v)], &mut replica) {
                         return Some(pos);
                     }
                 }
@@ -72,22 +60,17 @@ impl Quick {
 
 impl Solver for Quick {
     fn solve(&self, puzzle: &mut Sudoku) -> bool {
-        let filled: Vec<(usize, u8)> = puzzle
+        let mut filled: Vec<(usize, u8)> = puzzle
             .iter()
             .enumerate()
             .filter(|(_, &v)| v != 0)
             .map(|(i, v)| (i, *v))
             .collect();
-        let possible: Vec<Vec<u8>> = puzzle
+        let mut possible: Vec<Vec<u8>> = puzzle
             .iter()
             .map(|&v| if v == 0 { (1u8..=9).collect() } else { vec![v] })
             .collect();
-        let mut state = State {
-            worklist: filled,
-            // connections: self.connections.clone(),
-            possible: possible,
-        };
-        match self.proc(&mut state) {
+        match self.proc(&mut filled, &mut possible) {
             None => false,
             Some(pos) => {
                 pos.iter().enumerate().for_each(|(i, &v)| puzzle[i] = v);
